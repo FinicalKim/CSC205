9c1941c2dffe45aac7672cfacc18170a244bb2af
Kim P. Finical <79591944+FinicalKim@users.noreply.github.com>
2021-10-27 02:47:22 -0700

    Delete (default package) directory

--- (default package)/Tree.java
+++
@@ -1,7 +0,0 @@
-public interface Tree <T extends Comparable <T> >{
-	public void add(T item);
-	public boolean contains(T item);
-	public int size();
-	public void inOrderPrint();
-	public String toString();
-}
